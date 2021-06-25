ab91a835e24c487723e55f2440a38cc0a9498dea
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-25 02:35:36 -0500

    Updated base.js
    Added Firebase configuration.

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0P4WmKKG9vWA6SLb7PdAWa7lxL_GdWeI",
+    authDomain: "fire-base-practice-542e5.firebaseapp.com",
+    projectId: "fire-base-practice-542e5",
+    storageBucket: "fire-base-practice-542e5.appspot.com",
+    messagingSenderId: "904580328434",
+    appId: "1:904580328434:web:131210278580210621996b"
+  }
+
+  firebase.initializeApp(firebaseConfig)
+  export const auth = firebase.auth()
+
+  export default firebase
